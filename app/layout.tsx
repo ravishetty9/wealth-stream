60ad2febaa33fcd82e158559073edc2b9dca91c4
Ravish Shetty <ravishetty9@gmail.com>
2024-08-08 16:18:16 -0500

    implement initial file and folder structure

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter , IBM_Plex_Serif} from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const imbPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif'
+})
+
+export const metadata: Metadata = {
+  title: "WealthStream",
+  description: "WealthStream is a modern app that simplifies your financial management and facilitates seamless transactions with ease and efficiency.",
+  icons:{
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${imbPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
