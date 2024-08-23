2896796a913ba9ee0d3b32d3b977984472d34d08
Ravish Shetty <ravishetty9@gmail.com>
2024-08-23 15:33:04 -0500

    add force-dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,23 @@
+export const dynamic = "force-dynamic";
+
 import type { Metadata } from "next";
-import { Inter , IBM_Plex_Serif} from "next/font/google";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const imbPlexSerif = IBM_Plex_Serif({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-ibm-plex-serif'
-})
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-ibm-plex-serif",
+});
 
 export const metadata: Metadata = {
   title: "WealthStream",
-  description: "WealthStream is a modern app that simplifies your financial management and facilitates seamless transactions with ease and efficiency.",
-  icons:{
-    icon: '/icons/logo.svg'
-  }
+  description:
+    "WealthStream is a modern app that simplifies your financial management and facilitates seamless transactions with ease and efficiency.",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -24,7 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${imbPlexSerif.variable}`}>{children}</body>
+      <body className={`${inter.variable} ${imbPlexSerif.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
