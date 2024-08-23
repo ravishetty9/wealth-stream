3422dfeba33310dc5009f586ab938c3986b1d9d1
Ravish Shetty <ravishetty9@gmail.com>
2024-08-23 16:17:46 -0500

    fix some minor formatting

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,18 +2,21 @@ import HeaderBox from "@/components/HeaderBox";
 import RecentTransactions from "@/components/RecentTransactions";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
-import { getAccounts, getAccount } from "@/lib/actions/bank.actions";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
-import React from "react";
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
-  const accounts = await getAccounts({ userId: loggedIn.$id });
+  const accounts = await getAccounts({
+    userId: loggedIn.$id,
+  });
 
   if (!accounts) return;
+
   const accountsData = accounts?.data;
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
   const account = await getAccount({ appwriteItemId });
 
   return (
@@ -24,7 +27,7 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
             type="greeting"
             title="Welcome"
             user={loggedIn?.firstName || "Guest"}
-            subtext="Access and manage your account and transactions effeciently."
+            subtext="Access and manage your account and transactions efficiently."
           />
 
           <TotalBalanceBox
@@ -33,6 +36,7 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
             totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
+
         <RecentTransactions
           accounts={accountsData}
           transactions={account?.transactions}
